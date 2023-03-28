0ed108cc00916883cb87b168ffc6d1a8eebce774
Djafny <jafnyabdad@gmail.com>
2023-03-28 11:54:25 +0200

    v2

--- script.js
+++ script.js
@@ -3,4 +3,33 @@ function main() {
         .transition()
         .duration(1000)
         .style("background-color", "#0f095e");
+        
+    const canvas = d3.select("canvas").node();
+    const context = canvas.getContext("2d");
+    const radius = 50;
+    let x = radius;
+    let y = radius;
+    let dx = 1;
+    let dy = 1;
+
+    function animate() {
+        context.clearRect(0, 0, canvas.width, canvas.height);
+        context.beginPath();
+        context.arc(x, y, radius, 0, 2 * Math.PI);
+        context.stroke();
+
+        x += dx;
+        y += dy;
+
+        if (x + radius >= canvas.width || x - radius <= 0) {
+            dx = -dx;
+        }
+        if (y + radius >= canvas.height || y - radius <= 0) {
+            dy = -dy;
+        }
+
+        requestAnimationFrame(animate);
+    }
+
+    animate();
 }
